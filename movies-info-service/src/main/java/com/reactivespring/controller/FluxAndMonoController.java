a170379034ee0e0ea6d0a3c65a6ff32f7dafd1bd
mdashikadnan <md.ashikadnan@gmail.com>
2022-08-24 20:56:03 +1000

    Streaming endpoint and write test case for that

--- movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
@@ -1,9 +1,13 @@
 package com.reactivespring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.awt.*;
+import java.time.Duration;
 
 @RestController
 public class FluxAndMonoController {
@@ -14,5 +18,9 @@ public class FluxAndMonoController {
     @GetMapping("/mono")
     public Mono<String> helloWorldMono(){
         return Mono.just("hello-world").log();
+    }
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream(){
+        return Flux.interval(Duration.ofSeconds(1)).log();
     }
 }
