5d4026a320555d59bde69295c12249918611c42f
mdashikadnan <md.ashikadnan@gmail.com>
2022-08-31 21:03:50 +1000

    Bean validation functional web

--- movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
+++ movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
@@ -1,4 +1,123 @@
 package com.reactivespring.routes;
 
+import com.reactivespring.ReviewReactiveRepository;
+import com.reactivespring.domain.Review;
+import com.reactivespring.exceptionhandler.GlobalErrorHandler;
+import com.reactivespring.handler.ReviewsHandler;
+import com.reactivespring.router.ReviewRouter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ContextConfiguration(classes = {ReviewRouter.class, ReviewsHandler.class, GlobalErrorHandler.class})             //You can inject some beans automatically for you
+@AutoConfigureWebTestClient                                                             //These are the two beans that particular unit tests require
 public class ReviewsUnitTest {
+     @MockBean
+     private ReviewReactiveRepository reviewReactiveRepository;
+     @Autowired
+     private WebTestClient webTestClient;         //To interact with endpoints
+
+    @Test
+    void addReview() {
+        //given
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class))).thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+        //when
+        webTestClient
+                .post()
+                .uri("/v1/reviews")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewResponse ->{
+                    var savedReview = reviewResponse.getResponseBody();
+                    assert savedReview != null;
+                    assertNotNull(savedReview.getReviewId());
+                    assertEquals("abc", savedReview.getReviewId());
+
+                });
+    }
+
+    @Test
+    void addReview_validation() {
+        //given
+        var review = new Review(null, null, "Awesome Movie", -9.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class))).thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+        //when
+        webTestClient
+                .post()
+                .uri("/v1/reviews")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("rating.movieInfoId : must not be null, rating.negative : please pass a non-negative value");
+    }
+
+    @Test
+    void getAllReviews() {
+        //given
+        var reviewList = List.of(
+                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review(null, 1L, "Awesome Movie1", 9.0),
+                new Review(null, 2L, "Excellent Movie", 8.0));
+        when(reviewReactiveRepository.findAll()).thenReturn(Flux.fromIterable(reviewList));
+
+        //when
+        webTestClient
+                .get()
+                .uri("/v1/reviews")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Review.class)
+                .value(reviews -> {
+                    assertEquals(3, reviews.size());
+                });
+    }
+    @Test
+    void updateReview() {
+        //given
+
+        var reviewUpdate = new Review(null, 1L, "Not an Awesome Movie", 8.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class))).thenReturn(Mono.just(new Review("abc", 1L, "Not an Awesome Movie", 8.0)));
+        when(reviewReactiveRepository.findById((String) any())).thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+
+        //when
+        webTestClient
+                .put()
+                .uri("/v1/reviews/{id}", "abc")
+                .bodyValue(reviewUpdate)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Review.class)
+                .consumeWith(reviewResponse ->{
+                    var updatedReview = reviewResponse.getResponseBody();
+                    assert updatedReview != null;
+                    System.out.println("updatedReview : "+ updatedReview);
+                    assertEquals(8.0,updatedReview.getRating());
+                    assertEquals("Not an Awesome Movie", updatedReview.getComment());
+                });
+
+    }
 }
