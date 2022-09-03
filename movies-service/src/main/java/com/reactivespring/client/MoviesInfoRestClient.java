ce1504cfcc53a9a2febe80507944897a38713aaf
mdashikadnan <md.ashikadnan@gmail.com>
2022-09-03 17:44:00 +1000

    Stream client using webclient in movies service

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,5 +1,6 @@
 package com.reactivespring.client;
 
+import com.reactivespring.domain.Movie;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
 import com.reactivespring.exception.MoviesInfoServerException;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -51,6 +53,33 @@ public class MoviesInfoRestClient {
                     );
                 })
                 .bodyToMono(MovieInfo.class)
+                .retry(3)
+                .log();
+    }
+
+    public Flux<MovieInfo> retrieveMovieInfoStream() {
+        var url = moviesInfoUrl.concat("/stream");
+        return webClient
+                .get()
+                .uri(url)
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> { //Any kind of 4xx error is going to catch by this particular predicate
+                    log.info("Status code is : {}",clientResponse.statusCode().value());
+
+                    //Here it is accessing the actual error message from the body of the http response
+                    //And throw Mono related exception
+                    return clientResponse.bodyToMono(String.class).flatMap(
+                            responseMessage -> Mono.error(new MoviesInfoClientException(responseMessage, clientResponse.statusCode().value()))
+                    );
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> { //Any kind of 4xx error is going to catch by this particular predicate
+                    log.info("Status code is : {}",clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class).flatMap(
+                            responseMessage -> Mono.error(new MoviesInfoServerException(
+                                    "Server Exception in MoviesInfoService " +responseMessage))
+                    );
+                })
+                .bodyToFlux(MovieInfo.class)
                 .retry(3)
                 .log();
     }
